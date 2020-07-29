cc39c6186c3002bbc3ef5f7d6b80205450adeb6c
rishiar4 <therishabhsharma763@gmail.com>
2020-07-29 21:58:24 +0530

    Count Number of Digits

---
+++ Getting Started/Count Digits/README.md
@@ -0,0 +1,3 @@
+# Count digits in a number
+ 
+Print the number of digits in a number.
---
+++ Getting Started/Count Digits/count.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class count{
+    public static void main(String[] args) {
+        Scanner scn = new Scanner( System.in );
+        int n = scn.nextInt();
+
+        int digits = 0;
+
+        while( n > 0 )
+        {
+            n = n / 10;
+            digits++;
+        }
+        System.out.println( digits );
+    }
+}
