bb23aa4ec3404ea829cc4146d99f78966583da65
rishiar4 <therishabhsharma763@gmail.com>
2020-08-04 19:13:16 +0530

    Get Common Elements - 1

---
+++ Hashmap and Heaps/Get Common Elements - 1/README.md
@@ -0,0 +1,18 @@
+# Get Common Elements - 1
+
+1. You are given a number n1, representing the size of array a1.
+2. You are given n1 numbers, representing elements of array a1.
+3. You are given a number n2, representing the size of array a2.
+4. You are given n2 numbers, representing elements of array a2.
+5. You are required to print all elements of a2 which are also present in a1 (in order of their occurence in a2). Make sure to not print duplicates (a2 may have same value present many times)
+
+# Input Format
+
+A number n1
+n1 number of elements line separated.
+A number n2
+n2 number of elements line separated.
+
+# Output Format
+
+All relevant elements of a2 in separate lines (no duplicacy).
---
+++ Hashmap and Heaps/Get Common Elements - 1/Solution.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Solution{
+    public static void main(String[] args) {
+        Scanner scn = new Scanner( System.in );
+        
+        int n1 = scn.nextInt();
+        HashMap<Integer, Integer> hm = new HashMap<>();
+
+        for( int i = 0; i < n1; i++ )
+        {
+            int val = scn.nextInt();
+            if( hm.containsKey(val) == true )
+            {
+                hm.put(val,  1  );
+            }
+            else
+            {
+                hm.put( val , 1 );
+            }
+        }
+
+        int n2 = scn.nextInt();
+
+        for( int i = 0; i < n2; i++ )
+        {
+            int val = scn.nextInt();
+            if( hm.containsKey( val ) == true )
+            {
+                System.out.print( val +" ");
+                hm.remove( val );
+            }
+        }
+    }
+}
