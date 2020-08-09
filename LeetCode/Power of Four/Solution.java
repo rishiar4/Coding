9f51aba309cbc15976b36a9c20e324fe3989a026
rishiar4 <therishabhsharma763@gmail.com>
2020-08-10 00:42:25 +0530

    Power of Four

---
+++ LeetCode/Power of Four/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPowerOfFour(int num) {
+    
+        if( num == 0 || num == 2 || num == 3 )
+        {
+            return false;
+        }
+        
+        while( num != 1 )
+        {
+         if( num % 4 != 0 )
+         {
+             return false;
+         }
+         num = num / 4;
+        }
+        return true;
+        
+    }
+}
