77d8a5ea193c36d6966731a019f0ed3e2141bd60
rishiar4 <therishabhsharma763@gmail.com>
2020-08-10 00:40:07 +0530

    Power of Three

---
+++ LeetCode/Power of Three/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if( n == 0 || n == 2 )
+        {
+            return false;
+        }
+        
+        while( n != 1 )
+        {
+            if( n % 3 != 0 )
+                return false;
+            n = n / 3;
+        }
+        return true;
+    }
+}
