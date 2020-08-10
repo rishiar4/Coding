ac43c983d64efecc17da39d13e951ff7efe40a0b
rishiar4 <therishabhsharma763@gmail.com>
2020-08-10 13:43:42 +0530

    All Indices of Array

---
+++ Recursion/All Indices Of Array/Solution.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Solution{
+    public static void main(String[] args) {
+        Scanner scn = new Scanner( System.in );
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for( int i = 0; i < arr.length; i++ )
+        {
+            arr[i] = scn.nextInt();
+        }
+        int k = scn.nextInt();
+        printIndices( arr, 0 , k );
+    }
+
+    public static void printIndices( int[] arr , int index, int k )
+    {
+        if( arr.length == index )
+        {
+            return;
+        }
+        if( arr[ index ] == k  )
+        {
+            System.out.println( index );
+        }
+        
+        printIndices( arr, index + 1, k );
+        
+    }
+}
