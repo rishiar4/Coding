ee22503b94abedc9b66175587bbbd658b2574da5
rishiar4 <therishabhsharma763@gmail.com>
2020-08-11 14:42:25 +0530

    Recursive Binary Search

--- Searching/Binary Search/Recursive/Solution.java
+++ Searching/Binary Search/Recursive/Solution.java
@@ -10,6 +10,29 @@ public class Solution{
         {
             arr[i] = scn.nextInt();
         }
-        
+        int k = scn.nextInt();
+        int pos = bSearch( arr, k, 0, arr.length-1 );
+        System.out.println( pos );
+    }
+    
+    public static int bSearch( int[] arr, int k , int low, int high )
+    {
+        if( low > high )
+        {
+            return -1;
+        }
+        int mid = low + ( high - low )/2;
+        if( arr[ mid ] == k )
+        {
+            return mid;
+        }
+        else if( arr[ mid ] < k )
+        {
+            return bSearch( arr, k, mid + 1, high );
+        }
+        else
+        {
+            return bSearch( arr, k, low, mid - 1  );
+        }
     }
 }
