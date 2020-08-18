c116aedc4c060f287333aa0f2f6d511a8b20bab8
rishiar4 <therishabhsharma763@gmail.com>
2020-08-19 01:26:17 +0530

    Introduction

---
+++ Strings/Introduction/Solution.java
@@ -0,0 +1,17 @@
+
+public class Solution{
+    public static void main(String[] args) {
+        String str;
+
+        str = "Hello";
+
+        System.out.println(str);
+        System.out.println( str.length());
+
+        System.out.println( str.substring(1, 4) );
+        System.out.println( str.substring( 3 ) );
+
+        System.out.println( str.charAt( 0 ));
+
+    }
+}
