73d3152ec60b2d8c08d172adcee7d788415e4231
rishiar4 <therishabhsharma763@gmail.com>
2020-08-24 23:45:02 +0530

    Power Linear

--- Recursion/Power-linear/Solution.java
+++ Recursion/Power-linear/Solution.java
@@ -2,21 +2,18 @@ import java.io.*;
 import java.util.*;
 
 public class Solution{
-
     public static void main(String[] args) throws Exception {
         Scanner scn = new Scanner(System.in);
         int x = scn.nextInt();
         int n = scn.nextInt();
-        
-        System.out.println( power( x, n ) );
+        int ans = power( x, n );
+        System.out.println( ans );
     }
-
     public static int power(int x, int n){
         if( n == 0 )
         {
             return 1;
         }
-        
         return x * power( x, n - 1 );
     }
 
