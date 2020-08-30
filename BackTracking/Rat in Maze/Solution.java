4410aac9634c298797a1f97163dccce4e1cb8cb8
rishiar4 <therishabhsharma763@gmail.com>
2020-08-31 01:19:01 +0530

    Rat in Maze

--- BackTracking/Rat in Maze/Solution.java
+++ BackTracking/Rat in Maze/Solution.java
@@ -13,45 +13,44 @@ public class Solution{
         }
         System.out.println();
     }
-    public static int ratInMaze(  int sr, int sc, int[][] trap, int[][] ans )
+    public static int ratInMaze(  int sr, int sc, int[][] trap, int[][] ans, int jump )
     {
         if( sr == ans.length - 1 && sc == ans[0].length - 1 )
         {
-            ans[sr][sc] = 1;
+            ans[sr][sc] = jump;
             display( ans );
             ans[sr][sc] = 0;
             System.out.println();
             return 1;
         }
         int count = 0;
-        ans[ sr ][ sc ] = 1; 
+        ans[ sr ][ sc ] = jump++; 
 
         if( sr + 1 < trap.length && trap[ sr + 1 ][ sc ] == 0  )
         {
             // ans[ sr + 1 ][ sc ] = 1;
-            count += ratInMaze( sr + 1 , sc, trap, ans );
+            count += ratInMaze( sr + 1 , sc, trap, ans, jump );
             // ans[ sr + 1 ][ sc ] = 0; 
         }
 
         if( sc + 1 < trap[0].length && trap[ sr ][ sc + 1] == 0 )
         {
             // ans[ sr ][ sc + 1 ] = 1;
-            count += ratInMaze( sr, sc + 1, trap, ans );
-            // ans[ sr ][ sc + 1 ] = 0;  
+            count += ratInMaze( sr, sc + 1, trap, ans, jump );
         }
-
-        // trap[ sr ][ sc ] = 0;
+        
+        ans[ sr ][ sc ] = 0;  
         return count;
     }
     public static void solve()
     {
         int[][] trap = { 
-            {0, 1, 1, 1},
-            {0, 0, 1, 0},
-            {1, 0, 1, 1},
+            {0, 0, 0, 1},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
             {0, 0, 0, 0} };
             
-        System.out.println( ratInMaze( 0, 0, trap, new int[trap.length][trap[0].length] ) );
+        System.out.println( ratInMaze( 0, 0, trap, new int[trap.length][trap[0].length], 4 ) );
     }
     public static void main(String[] args) {
         solve();
