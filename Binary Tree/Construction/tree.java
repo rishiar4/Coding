6c486ac13f0fc2646d823e5aa411a10b16d40bcf
rishiar4 <therishabhsharma763@gmail.com>
2020-09-19 10:02:40 +0530

    Binary Tree Construction

---
+++ Binary Tree/Construction/tree.java
@@ -0,0 +1,53 @@
+public class tree{
+    
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node( int data )
+        {
+            this.data = data;
+        }
+    }
+
+    static int idx = 0;
+    public static Node constructTree( int[] arr )
+    {
+        if( idx >= arr.length || arr[idx] == -1 )
+        {
+            idx++;
+            return null;
+        }
+
+        Node node = new Node( arr[idx++] );
+        node.left = constructTree( arr );
+        node.right = constructTree( arr );
+        
+        return node;
+    }
+
+    public static void display( Node root )
+    {
+        if( root == null )
+        {
+            return;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(root.left == null ? "." : root.left.data );
+        sb.append(" <-- " + root.data + " --> " );
+        sb.append(root.right == null ? "."  : root.right.data );
+        System.out.println( sb );
+
+        display( root.left );
+        display( root.right );
+    }
+
+    public static void main( String[] args )
+    {
+        int[] arr = {10,20,40,-1,-1,50,80,-1,-1,90,-1,-1,30,60,100,-1,-1,-1,70,110,-1,-1,120,-1,-1};
+        Node root = constructTree( arr );
+        display( root );
+    }
+}
