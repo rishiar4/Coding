764d2b8a657d4206d0b74a7e2d135eeac2f75a99
rishiar4 <therishabhsharma763@gmail.com>
2020-09-24 02:05:44 +0530

    Display BST

---
+++ Binary Search Tree/Display/Solution.java
@@ -0,0 +1,52 @@
+public class Solution{
+
+    public static class Node{
+        int data;
+        Node left = null;
+        Node right = null;
+
+        public Node( int data )
+        {
+            this.data = data;
+        }
+    }
+
+    public static Node constructTree( int[] arr, int low, int high )
+    {
+        if( low > high ) return null;
+
+        int mid = low + ( high - low )/2;
+
+        Node node = new Node( arr[mid] );
+        node.left = constructTree( arr, low, mid - 1 );
+        node.right = constructTree( arr, mid + 1, high );
+
+        return node;
+    }
+
+    public static void display( Node root )
+    {
+        if( root == null ) return;
+        StringBuilder sb = new StringBuilder();
+
+        sb.append( root.left == null ? "." : root.left.data );
+        sb.append( " <-- " + root.data + " --> " );
+        sb.append( root.right == null ? "." : root.right.data );
+        System.out.println( sb.toString() );
+
+        display( root.left );
+        display( root.right );
+    }
+
+    public static void solve()
+    {
+        int[] arr = new int[14];
+        for( int i = 0; i < arr.length; i++ ) arr[i] = i * 10;
+        Node root = constructTree( arr, 0, arr.length - 1 );
+        display(root);
+    }
+    public static void main( String[] args )
+    {
+        solve();
+    }
+}
