50a4585c3c56670b3337ab96df4b5afe86533b30
rishiar4 <therishabhsharma763@gmail.com>
2020-10-06 12:29:09 +0530

    Display of Graphs

---
+++ Graphs/Display/display.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+
+public class display{
+    public static class Edge{
+        int v = 0;
+        int w = 0;
+
+        Edge( int v, int w )
+        {
+            this.v = v;
+            this.w = w;
+        }
+    }
+
+    static int N = 7;
+    static ArrayList<Edge>[] graph = new ArrayList[N];
+
+    public static void addEdge( int u, int v, int w )
+    {
+        graph[u].add( new Edge( v, w ) );
+        graph[v].add( new Edge( u, w ) );
+    }
+
+    public static void constructGraph()
+    {
+        for( int i = 0; i < N; i++ )
+            graph[i] = new ArrayList<>();
+        
+        addEdge( 0, 1, 10 );
+        addEdge( 0, 3, 10 );
+        addEdge( 1, 2, 10 );
+        addEdge( 2, 3, 40 );
+        addEdge( 3, 4, 2 );
+        addEdge( 4, 5, 2 );
+        addEdge( 4, 6, 8 );
+        addEdge( 5, 6, 3 );
+
+    }
+
+    public static void show()
+    {
+        for( int i = 0; i < N; i++ )
+        {
+            System.out.print( i + " --> ");
+            for( Edge e : graph[i] )
+            {
+                System.out.print( "( " + e.v + " , " + e.w + " ) " );
+            }
+            System.out.println();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        
+        constructGraph();
+
+        show();
+    }
+}
