170a1753d3a4475d86196a529569f471f194f646
rishiar4 <therishabhsharma763@gmail.com>
2020-11-07 23:13:28 +0530

    Max Envelopes Stack

--- Dynamic Programming/Max Envelopes Stack/Solution.java
+++ Dynamic Programming/Max Envelopes Stack/Solution.java
@@ -1,27 +1,27 @@
 import java.util.Arrays;
 public class Solution{
     public static void main(String[] args) {
-        int[][] arr = {{3, 6}, {5, 4}, {4, 8}, {6, 9}, {10, 7}, {12, 12}};
-        Arrays.sort( arr, (a,b) -> { 
+        int[][] ar = {{3, 6}, {5, 4}, {4, 8}, {6, 9}, {10, 7}, {12, 12}};
+        Arrays.sort( ar, (a,b) -> { 
             if( a[0] == b[0] ) return a[1] - b[1];
             else return a[0] - b[0];
         });
 
-        for( int[] d : arr ){
+        for( int[] d : ar ){
             for( int ele : d ){
                 System.out.print( ele + " ");
             }
             System.out.println();
         }
 
-        int n = arr.length;
+        int n = ar.length;
         int[] dp = new int[n];
 
         int maxLen = 0;
         for( int i = 0; i < n; i++ ){
             dp[i] = 1;
             for( int j = i - 1; j >= 0; j-- ){
-                if( arr[i][1] > arr[j][1] ){
+                if( ar[i][1] > ar[j][1] ){
                     dp[i] = Math.max( dp[i], dp[j] + 1);
                 }
             }
