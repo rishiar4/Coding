bcf6192aad6a3d8d45a7698f0c31ac4aa263b538
rishiar4 <therishabhsharma763@gmail.com>
2020-11-08 19:53:45 +0530

    647. Palindromic Substrings

---
+++ LeetCode/647. Palindromic Substrings/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        int count = 0;
+
+        for( int gap = 0; gap < n; gap++ ){
+            for( int i = 0, j = gap; j < n; j++, i++ ){
+                if( gap == 0 ) dp[i][j] = true;
+                else if( gap == 1 ) dp[i][j] = ( s.charAt(i) == s.charAt(j) );
+                else dp[i][j] = ( s.charAt(i) == s.charAt(j) && dp[i+1][j-1] );
+
+                if( dp[i][j] ) count++;
+            }
+        }
+
+        return count;
+    }
+}
