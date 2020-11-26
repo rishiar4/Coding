2fa3a95d915b39a273259308c5f0e65bc1d59dd1
rishiar4 <therishabhsharma763@gmail.com>
2020-11-26 16:30:41 +0530

    update

--- Organised/HashMapAndHeap/Heap/l001.java
+++ Organised/HashMapAndHeap/Heap/l001.java
@@ -1,4 +1,6 @@
-public classs l001{
+import java.util.ArrayList;
+
+public class l001{
 
     public static class Heap{
 
@@ -21,6 +23,8 @@ public classs l001{
             for( int i = n - 1; i >= 0; i-- ){
                 downHeapify( i );
             }
+
+            for( int ele : arr ) System.out.print( ele + " ");
         }
 
         public void downHeapify( int pi ){
@@ -28,7 +32,15 @@ public classs l001{
             int lci = 2 * pi + 1;
             int rci = 2 * pi + 2;
 
+            if( lci < this.arr.size() && this.arr.get( lci ) > this.arr.get( maxIdx ) )
+                maxIdx = lci;
+            if( rci < this.arr.size() && this.arr.get( rci ) > this.arr.get( maxIdx ) )
+                maxIdx = rci;
 
+            if( pi != maxIdx ){
+                swap( pi, maxIdx );
+                downHeapify( maxIdx );
+            }
         }
 
         private void swap( int a, int b ){
@@ -47,9 +59,11 @@ public classs l001{
             return this.arr.size();
         }
 
-        public void push(){
-
-        }
+        // public void push( int val ){
+        //     this.arr.add( val );
+        //     int n = this.arr.size();
+        //     upHeapify( n - 1 );
+        // }
 
         public int top(){
             return arr.get( 0 );
@@ -70,7 +84,6 @@ public classs l001{
     public static void solve(){
         int[] arr = { 10, 20, 30, -2, -3, -4, 5, 6, 7, 8, 9, 22, 11, 13 };
         Heap pq = new Heap( arr );
-
     }
     public static void main(String[] args){
         solve();
