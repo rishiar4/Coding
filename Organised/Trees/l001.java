3cb891697616ccd1189dabe58a62768c8d20f43d
rishiar4 <therishabhsharma763@gmail.com>
2020-11-28 02:01:57 +0530

    update

--- Organised/Trees/l001.java
+++ Organised/Trees/l001.java
@@ -38,11 +38,22 @@ public class l001{
         display( root.right );
     }
 
+    public static void inOrder( Node root ){
+        if( root == null ) return;
+
+        inOrder( root.left );
+
+        System.out.print( root.data + " ");
+
+        inOrder( root.right );
+    }
+
     public static void main( String[] args ){
         int[] arr = { 10,20,40,-1,-1,50,80,-1,-1,90,-1,-1,30,60,100,-1,-1,-1,70,110,-1,-1,120,-1,-1 };
 
         Node root = constructTree( arr );
 
         display( root );
+        inOrder( root );
     }
 }
