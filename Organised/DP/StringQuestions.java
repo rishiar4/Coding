25463dccfc7307cb90d4825d41e5fb795c18ef22
rishiar4 <therishabhsharma763@gmail.com>
2021-02-03 02:52:25 +0530

    update

--- Organised/DP/StringQuestions.java
+++ Organised/DP/StringQuestions.java
@@ -93,5 +93,81 @@ public class StringQuestions{
         return ans;
     }
 
+    // 115. Distinct Subsequences
+    public int numDistinct( String s, String t, int n, int m){
+        if( m == 0)
+            return 1;
+
+        if( n < m)
+            return 0;
+
+        int count = 0;
+        if( s.charAt( n - 1) == t.charAt( m - 1) ){
+            count += numDistinct( s, t, n - 1, m - 1);
+            count += numDistinct( s, t, n - 1, m);
+        }else{
+            count += numDistinct( s, t, n - 1, m);
+        }
+
+        return count;
+    }
+    public int numDistinct( String s, String t, int n, int m, int[][] dp){
+        if( m == 0){
+            return dp[n][m] = 1;
+        }
+
+        if( n < m){
+            return dp[n][m] = 0;
+        }
+
+        if( dp[n][m] != -1)
+            return dp[n][m];
+
+        int count = 0;
+        if( s.charAt(n-1) == t.charAt(m-1)){
+            count += numDistinct( s, t, n - 1, m - 1, dp);
+            count += numDistinct( s, t, n - 1, m, dp);
+        }else{
+            count += numDistinct( s, t, n - 1, m, dp);
+        }
+        return dp[n][m] = count;
+    }
+    public int numDistinctDP( String s, String t, int N, int M, int[][] dp){
+        for( int n = 0; n <= N; n++){
+            for( int m = 0; m <= M; m++){
+                if( m == 0){
+                    dp[n][m] = 1;
+                    continue;
+                }
+        
+                if( n < m){
+                    dp[n][m] = 0;
+                    continue;
+                }
+        
+                int count = 0;
+                if( s.charAt(n-1) == t.charAt(m-1)){
+                    count += dp[n-1][m-1]; //numDistinct( s, t, n - 1, m - 1, dp);
+                    count += dp[n-1][m]; //numDistinct( s, t, n - 1, m, dp);
+                }else{
+                    count += dp[n-1][m]; //numDistinct( s, t, n - 1, m, dp);
+                }
+                dp[n][m] = count;    
+            }
+        }
+        return dp[N][M];
+    }
+    public int numDistinct(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+
+        // int ans = numDistinct( s, t, n, m);
+        int[][] dp = new int[n+1][m+1];
+        for( int[] d : dp)
+            Arrays.fill( d, -1);
+        // int ans = numDistinct( s, t, n, m, dp);
+        int ans = numDistinctDP( s, t, n, m, dp);
+        return ans;
+    }
 
 }
